bc2820222db562672629d89fbeae848c0f39047a
Bonnie Simon <bonnie.developer@gmail.com>
2022-07-27 17:51:06 +0530

    Not working yet

--- cmd/ascii-art/main.go
+++ cmd/ascii-art/main.go
@@ -18,15 +18,39 @@ func getImageFromFilePath(path string) image.Image {
 	return img
 }
 
+func averageFilter(r, g, b uint32) int {
+	return (int(r) + int(g) + int(b)) / 3
+}
+
+func rgba2rgb(r, g, b, a uint32) {
+	red := a * r
+	green := a * g
+	blue := a * b
+	fmt.Println(red, " ", green, " ", blue)
+}
+
 func main() {
 	var imageFilePath string = "img.jpg"
 	img := getImageFromFilePath(imageFilePath)
-	fmt.Printf("Size: %d * %d\n", img.Bounds().Max.X, img.Bounds().Max.Y)
+
+	var imageHeight, imageWidth int = img.Bounds().Max.Y, img.Bounds().Max.X
+	fmt.Printf("Size: %d * %d\n", imageWidth, imageHeight)
+
+	// var brightnessMatrix [][]int
 
 	// Get Pixel values
 	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
 		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
-			fmt.Println(img.At(x, y))
+			fmt.Printf("%d\n", img.At(x, y))
+			r, g, b, a := img.At(x, y).RGBA()
+			rgba2rgb(r, g, b, a)
+			fmt.Println(((r >> 8) & 0xFF))
+			fmt.Printf("r: %T\n", r)
+			fmt.Printf("r: %d g: %d b: %d", r, g, b)
+			// fmt.Println(int(r), " ", int(g), " ", int(b))
+			fmt.Println(averageFilter(r, g, b))
+			break
 		}
+		break
 	}
 }
